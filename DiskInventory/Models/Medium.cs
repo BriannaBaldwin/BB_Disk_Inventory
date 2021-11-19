2157e1170d5b017fb2b03efc2c867203fe20ad27
Brianna Baldwin <73656451+BriannaBaldwin@users.noreply.github.com>
2021-11-19 11:40:57 -0700

    Updated filies
    Added validation and required

--- DiskInventory/Models/Medium.cs
+++ DiskInventory/Models/Medium.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -14,10 +15,16 @@ namespace DiskInventory.Models
         }
 
         public int MediaId { get; set; }
+        [Required(ErrorMessage = "Please enter media name.")]
         public string MediaName { get; set; }
+        [Required(ErrorMessage = "Please enter release date.")]
+        [Range(typeof(DateTime), "1/1/1950", "1/1/2050", ErrorMessage = "Year must be after 1950 and before 2050.")]
         public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Please choose media type.")]
         public int MediaTypeId { get; set; }
+        [Required(ErrorMessage = "Please choose genre.")]
         public int GenreId { get; set; }
+        [Required(ErrorMessage = "Please choose status.")]
         public int StatusId { get; set; }
 
         public virtual Genre Genre { get; set; }
